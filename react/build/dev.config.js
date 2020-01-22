19b26c8ad4fe3da05cd9ee4e162a33f97ab5d92c
yyyyjinying <1020066395@@qq.com>
2020-01-22 15:21:09 +0800

    all

--- react/build/dev.config.js
+++ react/build/dev.config.js
@@ -1,34 +1,34 @@
 /* eslint-disable no-undef */
-const path = require('path');
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require("path");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 // const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
 
 module.exports = merge(common, {
   entry: {
-    app: './src/index.jsx'
+    app: "./src/index.jsx",
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, '../dist')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "../dist"),
   },
-  devtool: 'inline-source-map', // 打包后是否可以定位bug
+  devtool: "inline-source-map", // 打包后是否可以定位bug
   mode: "development",
   devServer: {
-    contentBase: path.resolve(__dirname, '../dist'),
+    contentBase: path.resolve(__dirname, "../dist"),
     open: true,
     hot: true,
     compress: true,
-    port: 3030
+    port: 3030,
   },
   resolve: {
     extensions: [".js", ".tsx", ".ts", ".json", ".scss", "css"],
     alias: {
-      src: path.resolve(__dirname, '../src'),
-      component: path.resolve(__dirname, '../src/component'),
-      utils: path.resolve(__dirname, '../src/utils'),
+      src: path.resolve(__dirname, "../src"),
+      component: path.resolve(__dirname, "../src/component"),
+      utils: path.resolve(__dirname, "../src/utils"),
     },
-    modules: ['node_modules']
+    modules: ["node_modules"],
   },
   module: {
     rules: [
@@ -36,30 +36,30 @@ module.exports = merge(common, {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: [
-            {
-              loader: "babel-loader"
-            }
-        ]
-      },      
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
       {
         test: /\.(js|jsx)$/,
-        use:[
-            {
-              loader: 'eslint-loader'
-            }
+        use: [
+          {
+            loader: "eslint-loader",
+          },
         ],
         enforce: "pre", // 编译前检查
         exclude: /node_modules/, // 不检测的文件
-        include: [path.resolve(__dirname, '../src')], // 指定检查的目录
-        // options: { 
-          // emitError: true,
-          // formatter: "stylish"         
+        include: [path.resolve(__dirname, "../src")], // 指定检查的目录
+        // options: {
+        // emitError: true,
+        // formatter: "stylish"
         //   formatter: require("eslint-friendly-formatter") // community formatter
         // }
-      }
-    ]
+      },
+    ],
   },
   plugins: [
     // new AntdDayjsWebpackPlugin()
-  ]
-})
+  ],
+});
--- react/build/prod.config.js
+++ react/build/prod.config.js
@@ -1,18 +1,20 @@
 /* eslint-disable no-undef */
-const path = require('path');
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require("path");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
   entry: {
-    app: './src/index.jsx'
+    app: "./src/index.jsx",
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, '../dist')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "../dist"),
   },
-  mode: "production",
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
@@ -20,62 +22,102 @@ module.exports = merge(common, {
         // test: /\.js(\?.*)?$/i,  //测试匹配文件,
         // include: /\/includes/, //包含哪些文件
         // excluce: /\/excludes/, //不包含哪些文件
-        // //允许过滤哪些块应该被uglified（默认情况下，所有块都是uglified）。 
+        // //允许过滤哪些块应该被uglified（默认情况下，所有块都是uglified）。
         // //返回true以uglify块，否则返回false。
-        chunkFilter: (chunk) => {
+        chunkFilter: chunk => {
           // `vendor` 模块不压缩
-          if (chunk.name === 'vendor') {
+          if (chunk.name === "vendor") {
             return false;
           }
           return true;
-        }
-      })  
+        },
+      }),
+      new OptimizeCSSAssetsPlugin({}),
     ],
   },
-  devServer: {
-    contentBase: path.resolve(__dirname, '../dist'),
-    open: true,
-    hot: true,
-    compress: true,
-    port: 9000
-  },
+  mode: "production",
   resolve: {
     extensions: [".js", ".tsx", ".ts", ".json", ".scss", "css"],
     alias: {
-      src: path.resolve(__dirname, '../src'),
-      components: path.resolve(__dirname, '../src/components'),
-      utils: path.resolve(__dirname, '../src/utils'),
+      src: path.resolve(__dirname, "../src"),
+      component: path.resolve(__dirname, "../src/component"),
+      utils: path.resolve(__dirname, "../src/utils"),
     },
-    modules: ['node_modules']
+    modules: ["node_modules"],
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        test: /\.less$/,
         use: [
-            {
-              loader: "babel-loader"
-            }
-        ]
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            // options: {
+            //   importLoaders: 1,
+            //   modules: true,
+            //   localIndexName: "[name]__[local]___[hash:base64:5]", //配置class的名字
+            // },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: () => [
+                require("postcss-flexbugs-fixes"),
+                require("autoprefixer")({
+                  browsers: [
+                    ">1%",
+                    "last 4 versions",
+                    "Firefox ESR",
+                    "not ie < 9", // React doesn't support IE8 anyway
+                  ],
+                  flexbox: "no-2009",
+                }),
+              ],
+            },
+          },
+          {
+            loader: "less-loader",
+            options: {
+              javascriptEnabled: true,
+            },
+          },
+        ],
       },
       {
         test: /\.(js|jsx)$/,
-        use:[
-            {
-              loader: 'eslint-loader'
-            }
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: "eslint-loader",
+          },
         ],
         enforce: "pre", // 编译前检查
         exclude: /node_modules/, // 不检测的文件
-        include: [path.resolve(__dirname, '../src')], // 指定检查的目录
-        // options: { 
-          // emitError: true,
-          // formatter: "stylish"         
+        include: [path.resolve(__dirname, "../src")], // 指定检查的目录
+        // options: {
+        // emitError: true,
+        // formatter: "stylish"
         //   formatter: require("eslint-friendly-formatter") // community formatter
         // }
-      }
-    ]
+      },
+    ],
   },
-  plugins: []
-})
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+  ],
+});
