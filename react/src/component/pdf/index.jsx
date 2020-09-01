444cc9677a01ee35f4b6b4b8f144ec8b89a8a5f5
yyyyjinying <1020066395@@qq.com>
2020-09-01 09:21:08 +0800

    all

--- react/src/component/pdf/index.jsx
+++ react/src/component/pdf/index.jsx
@@ -1,8 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-// import { PDFReader, MobilePDFReader } from "reactjs-pdf-reader";
-// import { PDFReader, MobilePDFReader } from "react-read-pdf";
-import PDFReader from "@/component/base/pcPdf";
+import { PDFReader } from 'reactjs-pdf-reader-custom';
 class Test extends React.Component {
   constructor(props) {
     super(props);
@@ -12,8 +10,6 @@ class Test extends React.Component {
     return (
       <div style={{ overflow: "scroll", height: 600 }}>
         <PDFReader showAllPage={true} url="http://0.0.0.0:3030/common/file/test.pdf"/>
-        {/* <PDFReader url="http://0.0.0.0:3030/common/file/test.pdf" /> */}
-        {/* <MobilePDFReader page={2} url="/common/file/test.pdf" /> */}
       </div>
     );
   }
