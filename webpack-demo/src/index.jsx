489f954132ab3ebb1d68360e5e4c4313b001f4e8
yyyyjinying <1020066395@@qq.com>
2020-11-14 11:12:01 +0800

    all

--- webpack-demo/src/index.jsx
+++ webpack-demo/src/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
+import "./style.less";
+// import "./tapable";
+import Car from "./car.jpg";
 
-import "./tapable";
+const Img = new Image();
+Img.src = Car;
+
+document.body.appendChild(Img);
 
 render(<h1>2324466322</h1>, document.getElementById("app"));
